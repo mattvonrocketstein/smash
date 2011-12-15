db2242febc8d049e0c2c98b6fd1eccf2d3e1072d
matt <matt@cmg>
2011-12-15 11:16:46 -0500

    completers for git subcommands: add/mv

--- ipy_git_completers.py
+++ ipy_git_completers.py
@@ -1,10 +1,19 @@
-"""
-ipy_git_completers
+""" ipy_git_completers
+
+    TODO: subcommands 'mv' and 'add' should use file-system completion
 """
 import os
 import IPython.ipapi
 
 ip = IPython.ipapi.get()
+
+def set_complete(func,key):
+    ip.set_hook('complete_command', func, re_key=key)
+
+def filesystem_completer(self,event):
+    """ awkward, but cannot find a better way to do this.. """
+    base = __IPYTHON__.complete(event.line.split()[-1])
+    return [x for x in base if os.path.exists(x)]
 
 class git(object):
     @property
@@ -15,31 +24,16 @@ class git(object):
 
     @property
     def subcommands(self):
-        subs = ['add',
-                'bisect',
-                'branch',
-                'checkout',
-                'clone',
-                'commit',
-                'diff',
-                'fetch',
-                'grep',
-                'init',
-                'log',
-                'merge',
-                'mv',
-                'pull',
-                'push',
-                'rebase',
-                'reset',
-                'rm',
-                'show',
-                'status',
-                'tag']
+        subs = ['add', 'bisect', 'branch', 'checkout',
+                'cherry-pick' 'clone', 'commit', 'diff',
+                'fetch', 'grep', 'init', 'log', 'merge',
+                'mv', 'pull', 'push', 'rebase',
+                'reset', 'rm', 'show', 'status', 'tag']
+
         return subs
 git = git()
 
-ip.set_hook('complete_command',lambda self,event: git.subcommands,
-            re_key='git')
-ip.set_hook('complete_command',lambda self,event: git.local_branches,
-            re_key='git checkout')
+set_complete(lambda self, event: git.subcommands, 'git')
+set_complete(lambda self, event: git.local_branches, 'git checkout')
+set_complete(filesystem_completer, 'git add')
+set_complete(filesystem_completer, 'git mv')
