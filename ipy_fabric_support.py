095b187c22a55ef0d60d5df095515c7b38d6f542
matt <matt@cmg>
2012-02-10 18:30:09 -0500

    correcting ipy install procedure for ipython magic

--- ipy_fabric_support.py
+++ ipy_fabric_support.py
@@ -8,9 +8,10 @@ from ipy_bonus_yeti import post_hook_for_magic
 class magic_fabric(object):
     def __init__(self, lazy=True):
         if not lazy:
+            fabf=os.path.join(os.getcwd(),'fabfile.py')
             def thinkaboutit():
-                tmp = dict(__file__= 'fabfile.py')
-                execfile('fabfile.py', tmp)
+                tmp = dict(__file__= fabf)
+                execfile(fabf, tmp)
                 z = self.__published_methods
                 tmp = [ [k, tmp[k]] for k in tmp if k in z ]
                 for k,v in tmp:
@@ -57,11 +58,9 @@ class magic_fabric(object):
     @classmethod
     def install_into_ipython(kls):
         __IPYTHON__.shell.user_ns.update(_fabric=magic_fabric())
-
+        post_hook_for_magic('cd', look_for_fabfile)
 
 def look_for_fabfile():
     if 'fabfile.py' in os.listdir(os.getcwd()):
         print 'Discovered fabfile.  Type "_fabric?" to list commands'
         __IPYTHON__.shell.user_ns.update(_fabric=magic_fabric(lazy=False))
-
-post_hook_for_magic('cd', look_for_fabfile)
