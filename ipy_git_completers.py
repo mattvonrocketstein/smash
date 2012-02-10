4b90c5297ae863f07e196c1a2814677bff072cfb
matt <matt@cmg>
2012-02-10 16:52:51 -0500

    fix regex for git completer

--- ipy_git_completers.py
+++ ipy_git_completers.py
@@ -20,21 +20,31 @@ class git(object):
     @property
     def local_branches(self):
         """ """
-        return map(lambda x: x.replace("*","").strip(),
-                   os.popen('git branch -a|grep -v remote').readlines())
+        return filter(None, map(lambda x: x.replace("*","").strip(),
+                                os.popen('git branch -a|grep -v remote').readlines()))
 
     @property
     def subcommands(self):
         subs = ['add', 'bisect', 'branch', 'checkout',
-                'cherry-pick' 'clone', 'commit', 'diff',
+                'cherry-pick', 'clone', 'commit', 'diff',
                 'fetch', 'grep', 'init', 'log', 'merge',
                 'mv', 'pull', 'push', 'rebase',
                 'reset', 'rm', 'show', 'status', 'tag']
-
         return subs
 git = git()
 
-set_complete(lambda self, event: git.subcommands, 'git')
+
+set_complete(lambda self, event: git.subcommands, 'git [\S]*$')
 set_complete(lambda self, event: git.local_branches, 'git checkout')
+set_complete(lambda self, event: git.local_branches, 'git push')
 set_complete(filesystem_completer, 'git add')
 set_complete(filesystem_completer, 'git mv')
+
+def install_git_aliases():
+    __IPYTHON__.magic_alias('grc git rebase --continue')
+    __IPYTHON__.magic_alias('rebase git rebase -i')
+    __IPYTHON__.magic_alias('checkout git checkout')
+    __IPYTHON__.magic_alias('co git checkout')
+    __IPYTHON__.magic_alias('st git status')
+    __IPYTHON__.magic_alias('gd git diff --color')
+    __IPYTHON__.magic_alias('gc git commit')
