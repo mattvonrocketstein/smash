7b9e5f670a32d5c548ac772c55b320f15d5dd818
matt <matt@cmg>
2012-02-24 18:48:11 -0500

    minor changes

--- src/ipy_profile_msh.py
+++ src/ipy_profile_msh.py
@@ -7,6 +7,7 @@
 """
 import os
 import sys
+import inspect
 
 from IPython import ipapi
 
@@ -54,7 +55,7 @@ OVERRIDE_OPTIONS = dict(
     # will always complete attribute names starting with '_'.
     readline_omit__names = 1,
 
-    # uses emacs daemon to open files for objects as if by magic
+    # uses emacs daemon to open files for objects. as if by magic
     # try it out.. "%edit SomeModelClass" opens the file!
     editor = 'emacsclient')
 for option,val in OVERRIDE_OPTIONS.items():
@@ -63,7 +64,33 @@ for option,val in OVERRIDE_OPTIONS.items():
 report.msh('setting prompt')
 __IPYTHON__._cgb = lambda : os.popen("current_git_branch").read().strip()
 
+## general shell aliases.  this stuff is somewhat idiosyncratic, but it seems
+## like if it's not relevant it simply won't be used.  no harm no foul.
+################################################################################
+__IPYTHON__.magic_alias('dhclient sudo dhclient')
+__IPYTHON__.magic_alias('dad django-admin.py')
+__IPYTHON__.magic_alias('ls ls --color=auto')
+__IPYTHON__.magic_alias('apt-get sudo apt-get')
+
+
+## clean and begin main loop.  this first removes various common namespace
+## collisions between py-modules and unix shell commands. then we clean up the
+## strangeness of the command-line arguments (which are skewed due to the odd
+## way this script is invoked).  after it's clean, we can do last-minute
+## namespace manipulation and then start parsing on the
+## command-line and we're finished bootstrapping.
+################################################################################
+clean_namespace()
+sys.argv = sys.argv[1:]
+__IPYTHON__.user_ns.update(getfile=inspect.getfile)
+
 ## setup project manager
+################################################################################
+from ipy_project_manager import Project
+__manager__ = Project('__main__')
+__manager__._ipy_install()
+
+## idiosyncratic stuff.  TODO: this part should not be in this file!
 ################################################################################
 # consider every directory in ~/code to be a "project"
 # by default project.<dir-name> simply changes into that
@@ -71,37 +98,15 @@ __IPYTHON__._cgb = lambda : os.popen("current_git_branch").read().strip()
 # "start venv if present" or "show fab commands if present"
 from medley_ipy import load_medley_customizations
 from medley_ipy import load_medley_customizations2
-from ipy_project_manager import Project
-manager = Project('__main__')
 
 # for my personal projects and their customizations.
 # ( robotninja requires hammock's activation first )
-manager.bind_all(expanduser('~/code'))
-manager.pre_activate('robotninja',
-                     lambda: manager.activate(manager.hammock))
-manager._ipy_install()
-
-
-## general shell aliases
-################################################################################
-__IPYTHON__.magic_alias('dhclient sudo dhclient')
-__IPYTHON__.magic_alias('dad django-admin.py')
-__IPYTHON__.magic_alias('ls ls --color=auto')
-
-import inspect
-__IPYTHON__.user_ns.update(getfile=inspect.getfile)
+__manager__.bind_all(expanduser('~/code'))
+__manager__.pre_activate('robotninja',
+                         lambda: manager.activate(manager.hammock))
 
 # Medley specific things
-manager.bind(expanduser('~/jellydoughnut'))
-manager.bind_all(expanduser('~/devel'),
-                 post_activate=load_medley_customizations2,
-                 post_invoke=load_medley_customizations,)
-
-## clean and begin main loop.  this first removes various common namespace
-## collisions between py-modules and unix shell commands. then we clean up the
-## strangeness of the command-line arguments (which are skewed due to the odd
-## way this script is invoked).  after it's clean, we can start parsing on the
-## command-line and we're finished bootstrapping.
-################################################################################
-clean_namespace()
-import sys; sys.argv=sys.argv[1:]
+__manager__.bind(expanduser('~/jellydoughnut'))
+__manager__.bind_all(expanduser('~/devel'),
+                     post_activate=load_medley_customizations2,
+                     post_invoke=load_medley_customizations,)
