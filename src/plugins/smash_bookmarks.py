e54eb4a9b77a96421707b5f0d778c7052ac68013
matt <matt@cmg>
2012-09-27 23:12:28 -0400

    decruft and start from skeleton

---
+++ src/plugins/smash_bookmarks.py
@@ -0,0 +1,23 @@
+"""
+"""
+
+from smash.projects import Project, COMMAND_NAME
+from smash.util import report
+from smash.plugins import SmashPlugin
+
+class Bookmarks(object):
+    @property
+    def bookmarks(self):
+        p = __IPYTHON__.user_ns[COMMAND_NAME]._active_project
+        report.bm(str(p))
+
+
+class Plugin(SmashPlugin):
+    requires = []
+    report = report.bookmark_plugin
+
+    def install(self):
+        if 'bookmarks' in __IPYTHON__.user_ns:
+            self.report("bookmarks name already taken in shell namespace.  refusing to continue")
+        else:
+            __IPYTHON__.user_ns.update(bookmarks=Bookmarks())
