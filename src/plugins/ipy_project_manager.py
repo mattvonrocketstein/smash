b0e4c587f91954366e98d3d256adb2d50f5bb058
matt <matt@cmg>
2012-09-27 20:27:37 -0400

    refactor

--- src/plugins/ipy_project_manager.py
+++ src/plugins/ipy_project_manager.py
@@ -11,12 +11,30 @@ from smash.util import report
 from smash.python import opd, opj
 from smash.plugins import SmashPlugin
 from smash.util import post_hook_for_magic
-from smash.projects import Project, ROOT_PROJECT_NAME
+from smash.projects import Project, ROOT_PROJECT_NAME, COMMAND_NAME
 
 CONFIG_FILE_NAME = 'projects.json'
 
-
 class Plugin(SmashPlugin):
+
+    @staticmethod
+    def load_instructions(manager, config):
+        """ load instructions from json..
+
+            these typically represent function-calls to bind() and bind_all()
+        """
+        instructions = config.get('instructions', [])
+        for method_name, args, kargs in instructions:
+            getattr(manager, method_name)(*args, **kargs)
+
+    @staticmethod
+    def load_aliases(config):
+        default_aliases = config.get('aliases', {}).get(ROOT_PROJECT_NAME, [])
+        from smash import aliases
+        default_aliases = [ aliases.add(alias) for alias in default_aliases ]
+        report.project_manager('adding aliases: ' + str(default_aliases))
+        aliases.install()
+
     def install(self):
         config_file = opj(opd(opd(__file__)), CONFIG_FILE_NAME)
         Project._config_file = config_file
@@ -25,19 +43,17 @@ class Plugin(SmashPlugin):
             config = {}
             report.project_manager(' file does not exist')
         else:
-            config = demjson.decode(open(config_file,'r').read())
+            config = demjson.decode(open(config_file, 'r').read())
             report.project_manager(' config keys: '+str(config.keys()))
         manager = Project(ROOT_PROJECT_NAME)
 
         # dont move this next line.  post_activate/post_invoke things might want the manager.
-        __IPYTHON__.shell.user_ns['proj'] = manager
+        __IPYTHON__.shell.user_ns[COMMAND_NAME] = manager
 
         manager._config = config
-        instructions = config.get('instructions', [])
-        # consider every directory in ~/code to be a "project"
-        # by default proj.<dir-name> simply changes into that
-        for method_name, args, kargs in instructions:
-            getattr(manager, method_name)(*args, **kargs)
+        [ manager._add_post_activate(name, val) for name, val in config['post_activate'].items() ]
+
+        self.load_instructions(manager, config)
 
         # add option parsing for project-manager
         from smash.parser import SmashParser
@@ -49,13 +65,8 @@ class Plugin(SmashPlugin):
                                                                       opts.project).activate)
 
         # per-project aliases
-        default_aliases = config.get('aliases', {}).get(ROOT_PROJECT_NAME, [])
-        from smash import aliases
-        default_aliases = [ aliases.add(alias) for alias in default_aliases ]
-        report.project_manager('adding aliases: ' + str(default_aliases))
-        aliases.install()
+        self.load_aliases(config)
 
-        #raise Exception, aliases
         # install hooks in the environment
         # FIXME: this stopped working
         post_hook_for_magic('cd', manager._announce_if_project)
