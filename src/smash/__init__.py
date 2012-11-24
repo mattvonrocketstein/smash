4621ecc34b2a469bf553bb186d1c72919b054165
mvr <matthewvonrocketstein@gmail-dot-com>
2012-11-23 21:59:41 -0500

    compute config dir relative from smash-lib source

--- src/smash/__init__.py
+++ src/smash/__init__.py
@@ -6,3 +6,7 @@ from smash.plugins import Plugins
 from smash.aliases import Aliases
 aliases = Aliases()
 #plugins = Plugins()
+import os
+opd,opj = os.path.dirname, os.path.join
+config_dir = opj(opd(opd(__file__)), 'config')
+__all__ = [Plugins, Aliases, aliases, config_dir]
