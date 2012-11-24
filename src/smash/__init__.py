8c8506a7eb8a22a7df41ab6d1882a04fb25f4697
mvr <mvr@laptop>
2012-11-23 22:49:22 -0500

    plugins have names

--- src/smash/__init__.py
+++ src/smash/__init__.py
@@ -1,12 +1,17 @@
 """ SmaSh lib
 
-    TODO: dox
+    Things that will be set in this namespace after bootstrap:
+
+      * smash.PLUGINS :: a list of all installed plugins
+
 """
 from smash.plugins import Plugins
 from smash.aliases import Aliases
 aliases = Aliases()
-#plugins = Plugins()
+
 import os
 opd,opj = os.path.dirname, os.path.join
 config_dir = opj(opd(opd(__file__)), 'config')
+
+
 __all__ = [Plugins, Aliases, aliases, config_dir]
