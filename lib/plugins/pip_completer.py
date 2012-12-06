994b00da2629f4a77200ffa718b33ca2886ca574
matt <mvr@laptop>
2012-12-05 19:05:52 -0500

    pep

--- lib/plugins/pip_completer.py
+++ lib/plugins/pip_completer.py
@@ -4,17 +4,18 @@
     additional "pip install <tab>" will complete from
     requirements.txt if it is in the working directory.
 """
-
-
 import os
+
 from smashlib.plugins import SmashPlugin
 from smashlib.util import report, set_complete
+
 PIP_CMDS = 'bundle freeze help install search uninstall unzip zip'.split()
 
 def pip_complete(self, event):
     return PIP_CMDS
 
 def requirements_complete(self, event):
+    """ TODO: even smarter.. check $event and maybe use filesystem completer """
     if 'requirements.txt' in os.listdir(os.getcwd()):
         reqs = [x.strip() for x in open('requirements.txt').readlines() ]
         reqs = [x for x in reqs if not x.startswith('#')]
