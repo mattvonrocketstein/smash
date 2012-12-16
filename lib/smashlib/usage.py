3ca9ef19940e3a067adf894c749262a8cd09a96c
matt <mvr@laptop>
2012-12-16 12:02:59 -0500

    minorchanges

--- lib/smashlib/usage.py
+++ lib/smashlib/usage.py
@@ -1,7 +1,10 @@
 """
 Smash: the smart shell
 
+  to view the smash metadata such as installation directory, configuration files just type {red}smashlib._meta{normal}.
+
   for help with plugins type: {red}plugins?{normal}
   for help with projects type: {red}projects?{normal}
+  for help with aliases type: {red}aliases?{normal}
 
 """
