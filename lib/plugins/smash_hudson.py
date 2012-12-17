11c5bfaed03ae97ea42cf44ce765de8375746920
mvr <matthewvonrocketstein@gmail-dot-com>
2012-12-17 10:07:21 -0500

    placeholder for hudson

---
+++ lib/plugins/smash_hudson.py
@@ -0,0 +1,7 @@
+""" smash_hudson:
+
+        plugin for interacting with the hudson build bot.
+        jenkins actually supports nice CLI tools, but hudson
+        not so much.  this utility will help to scrape whatever
+        it can from the api
+"""
