fd0200deadf1d8c1c7648b4ce127f209f97de537
mvr <matthewvonrocketstein@gmail-dot-com>
2012-12-26 15:18:32 -0500

    minor changes

--- lib/smashlib/__init__.py
+++ lib/smashlib/__init__.py
@@ -24,11 +24,15 @@ _meta = dict( config_dir = opj(opd(opd(__file__)), 'etc'),
 _meta.update(smash_rc=opj(_meta['config_dir'], 'smash.rc'))
 
 def fac(m):
-    return lambda bus, *args, **kargs: print(m + str(kargs))
+    from smashlib.util import report
+    return lambda bus, *args, **kargs: getattr(report,m)(str(kargs))
 
 
 bus = Bus()
-bus.subscribe('post_invoke',   fac('post_invoke:'))
+bus.subscribe('post_invoke',   fac('post_invoke'))
 bus.subscribe('pre_invoke',    fac('pre_invoke'))
 bus.subscribe('pre_activate',  fac('pre_activate'))
 bus.subscribe('post_activate', fac('post_activate'))
+
+bus.subscribe('pre_deactivate',  fac('pre_deactivate'))
+bus.subscribe('post_deactivate', fac('post_deactivate'))
