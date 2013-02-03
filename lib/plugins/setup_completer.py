8a871663a6e31f3609a107f91c710988b401425c
matt <mvr@laptop>
2013-02-03 07:09:36 -0500

    sq

--- lib/plugins/setup_completer.py
+++ lib/plugins/setup_completer.py
@@ -1,10 +1,15 @@
 """ simple completion for python setup.py
 
-    want to trigger on:
+    want to trigger completion for stuff like this:
       python setup.py<tab>
+      ./python setup.py<tab>
       python ./setup.py<tab>
       python2.7 ./setup.py<tab>
       .. etc
+
+    additionally, because setup.py might add new scripts to
+    bin directories, always trigger a rehash after setup.py
+    is invoked
 """
 
 from smashlib.util import set_complete, add_hook
@@ -13,7 +18,9 @@ from IPython.ipapi import TryNext
 
 setup_re = 'python.*setup.py'
 
-def new_hook(*args, **kargs):
+# TODO: something very much like this
+#       after running 'make install' or apt
+def setup_py_hook(*args, **kargs):
     last_line  =__IPYTHON__.user_ns['In'][-1]
     sys_call_start = '_ip.system('
     if last_line.startswith(sys_call_start):
@@ -22,9 +29,10 @@ def new_hook(*args, **kargs):
         # get the thing inbetween the quotes
         sys_cmd = last_line.split(quote_char)[1]
         sys_cmd = sys_cmd.strip()
-        if 'python' in sys_cmd and \
-           'setup.py' in sys_cmd:
-            report.setup_py("detected that you ran setup.py.. rehashing env")
+        # FIXME: probably just use setup_re
+        if 'python' in sys_cmd and 'setup.py' in sys_cmd:
+            report.setup_py("detected that you ran setup.py.. "
+                            "rehashing env")
             __IPYTHON__.magic_rehashx()
     raise TryNext()
 
@@ -32,4 +40,6 @@ class Plugin(SmashPlugin):
     def install(self):
         completer = lambda *args,**kargs: ['build', 'develop','install']
         set_complete(completer, setup_re)
-        self.add_hook('generate_prompt', new_hook, 0)
+        # the specific hook choice and priority here is pretty
+        # arbitrary.. whatever it seems to work.
+        self.add_hook('generate_prompt', setup_py_hook, 0)
