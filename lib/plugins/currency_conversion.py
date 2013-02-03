0191ac5e0fc5c24ab461f9641c089271af2a0e0f
mvr <matthewvonrocketstein@gmail-dot-com>
2013-02-03 07:04:26 -0500

    detect when setup.py runs and rehash env

--- lib/plugins/currency_conversion.py
+++ lib/plugins/currency_conversion.py
@@ -14,13 +14,13 @@ import demjson
 import IPython
 import IPython.ipapi
 
-from smashlib.util import report, do_it_later
+from smashlib.util import report, do_it_later, _ip
 from smashlib.smash_plugin import SmashPlugin
 
 CODES = {"AED":"United Arab Emirates Dirham (AED)","ANG":"Netherlands Antillean Guilder (ANG)","ARS":"Argentine Peso (ARS)","AUD":"Australian Dollar (AUD)","BDT":"Bangladeshi Taka (BDT)","BGN":"Bulgarian Lev (BGN)","BHD":"Bahraini Dinar (BHD)","BND":"Brunei Dollar (BND)","BOB":"Bolivian Boliviano (BOB)","BRL":"Brazilian Real (BRL)","BWP":"Botswanan Pula (BWP)","CAD":"Canadian Dollar (CAD)","CHF":"Swiss Franc (CHF)","CLP":"Chilean Peso (CLP)","CNY":"Chinese Yuan (CNY)","COP":"Colombian Peso (COP)","CRC":"Costa Rican Colón (CRC)","CZK":"Czech Republic Koruna (CZK)","DKK":"Danish Krone (DKK)","DOP":"Dominican Peso (DOP)","DZD":"Algerian Dinar (DZD)","EEK":"Estonian Kroon (EEK)","EGP":"Egyptian Pound (EGP)","EUR":"Euro (EUR)","FJD":"Fijian Dollar (FJD)","GBP":"British Pound Sterling (GBP)","HKD":"Hong Kong Dollar (HKD)","HNL":"Honduran Lempira (HNL)","HRK":"Croatian Kuna (HRK)","HUF":"Hungarian Forint (HUF)","IDR":"Indonesian Rupiah (IDR)","ILS":"Israeli New Sheqel (ILS)","INR":"Indian Rupee (INR)","JMD":"Jamaican Dollar (JMD)","JOD":"Jordanian Dinar (JOD)","JPY":"Japanese Yen (JPY)","KES":"Kenyan Shilling (KES)","KRW":"South Korean Won (KRW)","KWD":"Kuwaiti Dinar (KWD)","KYD":"Cayman Islands Dollar (KYD)","KZT":"Kazakhstani Tenge (KZT)","LBP":"Lebanese Pound (LBP)","LKR":"Sri Lankan Rupee (LKR)","LTL":"Lithuanian Litas (LTL)","LVL":"Latvian Lats (LVL)","MAD":"Moroccan Dirham (MAD)","MDL":"Moldovan Leu (MDL)","MKD":"Macedonian Denar (MKD)","MUR":"Mauritian Rupee (MUR)","MVR":"Maldivian Rufiyaa (MVR)","MXN":"Mexican Peso (MXN)","MYR":"Malaysian Ringgit (MYR)","NAD":"Namibian Dollar (NAD)","NGN":"Nigerian Naira (NGN)","NIO":"Nicaraguan Córdoba (NIO)","NOK":"Norwegian Krone (NOK)","NPR":"Nepalese Rupee (NPR)","NZD":"New Zealand Dollar (NZD)","OMR":"Omani Rial (OMR)","PEN":"Peruvian Nuevo Sol (PEN)","PGK":"Papua New Guinean Kina (PGK)","PHP":"Philippine Peso (PHP)","PKR":"Pakistani Rupee (PKR)","PLN":"Polish Zloty (PLN)","PYG":"Paraguayan Guarani (PYG)","QAR":"Qatari Rial (QAR)","RON":"Romanian Leu (RON)","RSD":"Serbian Dinar (RSD)","RUB":"Russian Ruble (RUB)","SAR":"Saudi Riyal (SAR)","SCR":"Seychellois Rupee (SCR)","SEK":"Swedish Krona (SEK)","SGD":"Singapore Dollar (SGD)","SKK":"Slovak Koruna (SKK)","SLL":"Sierra Leonean Leone (SLL)","SVC":"Salvadoran Colón (SVC)","THB":"Thai Baht (THB)","TND":"Tunisian Dinar (TND)","TRY":"Turkish Lira (TRY)","TTD":"Trinidad and Tobago Dollar (TTD)","TWD":"New Taiwan Dollar (TWD)","TZS":"Tanzanian Shilling (TZS)","UAH":"Ukrainian Hryvnia (UAH)","UGX":"Ugandan Shilling (UGX)","USD":"US Dollar (USD)","UYU":"Uruguayan Peso (UYU)","UZS":"Uzbekistan Som (UZS)","VEF":"Venezuelan Bolívar (VEF)","VND":"Vietnamese Dong (VND)","XOF":"CFA Franc BCEAO (XOF)", "YER":"Yemeni Rial (YER)","ZAR":"South African Rand (ZAR)","ZMK":"Zambian Kwacha (ZMK)",}
 
 CACHE = {}
-ip = IPython.ipapi.get()
+
 # matcher for stuff like 100 usd to eur, etc
 PATTERN = re.compile('\d* \w\w\w to \w\w\w')
 url_t = 'http://rate-exchange.appspot.com/currency?from={0}&to={1}'
@@ -39,7 +39,7 @@ def base_rate(c1, c2):
         except KeyError: raise Exception,obj
         else:            CACHE[(c1, c2)] = rate
 
-    return CACHE[(c1,c2)]
+    return CACHE[(c1, c2)]
 
 def handler(line, mo):
     val,c1,_,c2 = filter(None, line.split())
@@ -50,16 +50,20 @@ def handler(line, mo):
     report('retrieved base-rate: {0}'.format(rate))
     return 'smashlib.util.report("{0} {1}")'.format(result, c2.upper())
 
-
-
 class Plugin(SmashPlugin):
     def install(self):
-        meta = ip.meta
-        def add_prefilter():
-            try:
-                meta.re_prefilters.append([PATTERN, handler])
-            except AttributeError:
+        meta = _ip().meta
+
+        def add_prefilter(count=0):
+            # this is awkward, but i need to sidestep running a race.
+            # and no, the 'late_startup_hook' did not help either :(
+            if count > 3:
                 report.ERROR("could not install currency-converter plugin.. "
-                       "re_prefilter still not set")
+                             "re_prefilter still not set")
+            else:
+                try:
+                    meta.re_prefilters.append([PATTERN, handler])
+                except AttributeError:
+                    add_prefilter(count+1)
 
         do_it_later(add_prefilter)
--- lib/plugins/setup_completer.py
+++ lib/plugins/setup_completer.py
@@ -7,12 +7,29 @@
       .. etc
 """
 
-from smashlib.util import set_complete
+from smashlib.util import set_complete, add_hook
 from smashlib.smash_plugin import SmashPlugin
+from IPython.ipapi import TryNext
 
 setup_re = 'python.*setup.py'
+
+def new_hook(*args, **kargs):
+    last_line  =__IPYTHON__.user_ns['In'][-1]
+    sys_call_start = '_ip.system('
+    if last_line.startswith(sys_call_start):
+        # might be " or '.  TODO: regex
+        quote_char = last_line[len(sys_call_start):][0]
+        # get the thing inbetween the quotes
+        sys_cmd = last_line.split(quote_char)[1]
+        sys_cmd = sys_cmd.strip()
+        if 'python' in sys_cmd and \
+           'setup.py' in sys_cmd:
+            report.setup_py("detected that you ran setup.py.. rehashing env")
+            __IPYTHON__.magic_rehashx()
+    raise TryNext()
 
 class Plugin(SmashPlugin):
     def install(self):
         completer = lambda *args,**kargs: ['build', 'develop','install']
         set_complete(completer, setup_re)
+        self.add_hook('generate_prompt', new_hook, 0)
