d03df4ac931dc7242fcf87abd80e2baaa29e9274
matt <mvr@laptop>
2013-03-31 09:57:11 -0400

    bugfix promptcomponents, bus

--- lib/plugins/host_prompt.py
+++ lib/plugins/host_prompt.py
@@ -1,11 +1,13 @@
 """ host_prompt
 """
 import platform
-from smashlib.prompt import prompt
+from smashlib.prompt import prompt, PromptComponent
 from smashlib.smash_plugin import SmashPlugin
 
 class Plugin(SmashPlugin):
     """ """
     def install(self):
         report('installing host-prompt')
-        prompt['host'] = [1, platform.node()]
+        prompt.add(
+            PromptComponent(name='host',priority=1,
+                            template=platform.node()))
--- lib/smashlib/__init__.py
+++ lib/smashlib/__init__.py
@@ -13,8 +13,7 @@ from __future__ import print_function
 import sys, types
 
 
-from cyrusbus import Bus
-
+from smashlib.bus import bus
 from smashlib.plugin_manager import PluginManager
 from smashlib.aliases import Aliases
 from smashlib.python import opd, opj, ope, mkdir
@@ -27,19 +26,7 @@ active_plugins = sys.modules['smashlib.active_plugins'] = types.ModuleType('smas
 _meta = dict( config_dir = opj(opd(opd(__file__)), 'etc'),
               bin_dir    = opj(opd(opd(__file__)), 'bin'),
               tmp_dir    = opj(opd(opd(__file__)), 'tmp'), )
+
 if not ope(_meta['tmp_dir']):
     mkdir(_meta['tmp_dir'])
 _meta.update(smash_rc=opj(_meta['config_dir'], 'smash.rc'))
-
-def fac(m):
-    from smashlib.util import report_if_verbose
-    return lambda bus, *args, **kargs: getattr(report_if_verbose, m)(str(kargs) )
-
-
-bus = Bus()
-bus.subscribe('post_invoke',   fac('post_invoke'))
-bus.subscribe('pre_invoke',    fac('pre_invoke'))
-bus.subscribe('pre_activate',  fac('pre_activate'))
-bus.subscribe('post_activate', fac('post_activate'))
-bus.subscribe('pre_deactivate',  fac('pre_deactivate'))
-bus.subscribe('post_deactivate', fac('post_deactivate'))
