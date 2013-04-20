2b7aff2592e2eeadf28139bebf4d397c57a8c18d
mvr <matthewvonrocketstein@gmail-dot-com>
2013-04-20 08:34:24 -0400

    better reporting

--- lib/plugins/host_prompt.py
+++ lib/plugins/host_prompt.py
@@ -7,7 +7,7 @@ from smashlib.smash_plugin import SmashPlugin
 class Plugin(SmashPlugin):
     """ """
     def install(self):
-        report('installing host-prompt')
+        report.host_prompt('installing')
         prompt.add(
             PromptComponent(name='host',priority=1,
                             template=platform.node()))
