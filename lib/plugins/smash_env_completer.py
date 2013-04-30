c5daf54cd2bc5e44b3166829cdf7bc9b9fcf5569
matt <mvr@laptop>
2013-04-30 17:14:19 -0400

    env completer

---
+++ lib/plugins/smash_env_completer.py
@@ -0,0 +1,13 @@
+""" smash_env_completer
+
+    Completes stuff along the lines of "echo $VIRTUAL_EN<tab>"
+"""
+from smashlib.smash_plugin import SmashPlugin
+from smashlib.util import report, set_complete
+
+def env_completer(self, event):
+    return os.environ.keys()
+
+class Plugin(SmashPlugin):
+    def install(self):
+        set_complete(env_completer, '.* .*\$')
