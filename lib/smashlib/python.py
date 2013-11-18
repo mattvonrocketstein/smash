47e6749cb16f52f9cc95d5818ac88c18d609c36a
mvr <matthewvonrocketstein@gmail-dot-com>
2013-11-18 09:10:45 -0500

    more python import shortcuts

--- lib/smashlib/python.py
+++ lib/smashlib/python.py
@@ -5,17 +5,20 @@
 import os
 import glob
 
-makedir = mkdir = os.mkdir
-opj = os.path.join
-ope = os.path.exists
-ops = os.path.split
-opd = os.path.dirname
+opj        = os.path.join
+ope        = os.path.exists
+ops        = os.path.split
+opd        = os.path.dirname
+listdir    = os.listdir
+isdir      = os.path.isdir
+makedir    = mkdir = os.mkdir
 splitext   = os.path.splitext
 abspath    = os.path.abspath
-expanduser = os.path.expanduser
 getcwd     = os.getcwd
 glob       = glob.glob
 get_env    = os.environ.get
+expanduser = os.path.expanduser
+
 def only_py_files(dir, rel=False):
     """ """
     result = glob(opj(dir, '*.py'))
