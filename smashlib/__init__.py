c0ca65e411987c4b76b6912e2612e640f3798587
mvr <matthewvonrocketstein@gmail-dot-com>
2014-11-17 10:18:26 +0000

    fix smashlib.embed()

--- smashlib/__init__.py
+++ smashlib/__init__.py
@@ -3,9 +3,15 @@
 __version__ = 0.1
 
 def start_ipython(argv=None, **kwargs):
+    #from smashlib.python import ModPath
+    #modpath = ModPath.install()
     from smashlib.overrides import launch_new_instance
     return launch_new_instance(argv=argv, **kwargs)
 
-def embed(argv):
-    #from IPython import start_ipython
-    start_ipython(argv=argv)
+def embed(argv=tuple(), **kargs):
+    from goulash._inspect import get_caller
+    context = kargs.pop('user_ns', {})
+    caller_context = get_caller(2)
+    context.update(caller_context['globals'])
+    context.update(caller_context['locals'])
+    start_ipython(argv=argv, user_ns=context, **kargs)
