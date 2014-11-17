14f222f7917e7696da408819450f3a71e1ef01bf
mvr <matthewvonrocketstein@gmail-dot-com>
2014-11-17 23:24:54 +0000

    add version info

--- setup.py
+++ setup.py
@@ -10,8 +10,16 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 if not os.getcwd()==this_dir:
     os.chdir(this_dir)
 
+# make sure we can import the version number so that it doesn't have
+# to be changed in two places. smashlib/__init__.py is also free
+# to import various requirements that haven't been installed yet
+sys.path.append(os.path.join(this_dir, 'smashlib'))
+from version import __version__
+sys.path.pop()
+
 setup(
     name         = 'smashlib',
+    version      = __version__,
     author       = 'mattvonrocketstein',
     author_email = '$author@gmail',
     version      = '0.1',
