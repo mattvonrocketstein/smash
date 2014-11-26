f799432fda995366140340dbc8939c590acaa959
mvr <matthewvonrocketstein@gmail-dot-com>
2014-11-26 03:44:30 +0000

    sp bash functions

--- smashlib/util/bash.py
+++ smashlib/util/bash.py
@@ -27,5 +27,36 @@ def get_aliases():
         aliases.append([alias,cmd])
     return aliases
 
+def get_functions():
+    cmd = '''bash -c "echo 'echo MARKER;compgen -A function;echo MARKER'|bash -i"'''
+    p1 = Popen(cmd, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    out, err = p1.communicate()
+    lines = out.split('\n')
+    lines = [ x.strip() for x in lines ]
+    lines = [ x for x in lines if x and not x.startswith('_') ]
+    lines = [ x for x in lines if re.compile('\w*').match(x)]
+    lines = lines[lines.index('MARKER')+1:]
+    lines = lines[:lines.index('MARKER')]
+    function_names = lines
+    return function_names
+
+from report import report
+def bash_function(fxn_name, input_string, quiet=False):
+    """ if you have quoted values in input_string, this will probably break"""
+    report("Running: {0} {1}".format(fxn_name, input_string))
+    cmd = '''bash -c "echo 'echo MARKER;{0} {1};echo MARKER'|bash -i"'''
+    cmd = cmd.format(fxn_name, input_string)
+    p1 = Popen(cmd, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    out, err = p1.communicate()
+    lines = out.split('\n')
+    lines = [ x.strip() for x in lines ]
+    lines = lines[lines.index('MARKER')+1:]
+    lines = lines[:lines.index('MARKER')]
+    if not quiet:
+        print '\n'.join(lines)
+    return lines
+
 if __name__=='__main__':
-    get_aliases()
+    #get_aliases()
+    print get_functions()
+    print bash_function('checkport', '8080')
