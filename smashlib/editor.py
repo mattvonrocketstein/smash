8886ca902b8e551f671b889c6f54d19a05e6467c
mvr <matthewvonrocketstein@gmail-dot-com>
2014-12-07 05:15:34 +0000

    switch to using voluptuous validation everywhere

--- smashlib/editor.py
+++ smashlib/editor.py
@@ -9,14 +9,10 @@ from IPython.terminal.interactiveshell import get_default_editor
 
 from smashlib.data import EDITOR_CONFIG_PATH
 from smashlib.util import touch_file
+from smashlib.config import SmashConfig
+#from smashlib.config.schemas import EDITOR_SCHEMA
 
-EDITOR_SCHEMA = {
-     "type" : "object",
-     "properties" : {
-         "console" : {"type" : "string"},
-         "window_env" : {"type" : "string"},
-     },
- }
+#def validate(data, schema):
 
 def get_example(schema):
     props = [x for x in schema['properties']]
@@ -33,34 +29,12 @@ def get_editor():
     user_editor = None
     sys_editor = get_default_editor()
     is_windowing_env = lambda: True # placeholder
-    if os.path.exists(EDITOR_CONFIG_PATH):
-        #with open(EDITOR_CONFIG_PATH) as fhandle:
-        #    user_editor = fhandle.read().strip()
 
-        try:
-            user_editor = demjson.decode_file(EDITOR_CONFIG_PATH)
-            jsonschema.validate(user_editor, EDITOR_SCHEMA)
-        except demjson.JSONDecodeError:
-            print '... warning: found {0} but cannot decode the data'.format(EDITOR_CONFIG_PATH)
-        except jsonschema.ValidationError:
-            err = ('JSON in {0} does not validate.\n'
-                   '\nhere is an example of correct data:\n\n')+\
-                   str(get_example(EDITOR_SCHEMA))
-            raise SystemExit(err)
-        else:
-            print "...found editor config in "+EDITOR_CONFIG_PATH
-            if is_windowing_env:
-                user_editor = user_editor['window_env']
-            else:
-                user_editor = user_editor['console']
+    c = SmashConfig()
+    econfig = c.load_from_etc('editor.json')
+    if is_windowing_env:
+        user_editor = econfig['window_env']
     else:
-        touch_file(EDITOR_CONFIG_PATH)
-        with open(EDITOR_CONFIG_PATH, 'w') as fhandle:
-            fhandle.write(demjson.encode(dict(
-                console=sys_editor,
-                window_env=sys_editor)))
-        print "... no editor configs are located at '{0}'.".format(EDITOR_CONFIG_PATH)
-        print ".... created template using {0}".format(sys_editor)
-        user_editor = None
+        user_editor = econfig['console']
     editor = user_editor or sys_editor or 'vi'
     return editor
