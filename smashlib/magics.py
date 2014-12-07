f8e16ee995d1d78c76ca3ea74d97b54d5d39455d
mvr <matthewvonrocketstein@gmail-dot-com>
2014-12-07 05:57:52 +0000

    ed_aliases shortcut

--- smashlib/magics.py
+++ smashlib/magics.py
@@ -4,6 +4,7 @@
 from IPython.core.magic import Magics, magics_class, line_magic
 
 from smashlib.data import USER_CONFIG_PATH, EDITOR_CONFIG_PATH
+from smashlib.data import ALIAS_CONFIG_PATH
 
 
 @magics_class
@@ -13,6 +14,10 @@ class SmashMagics(Magics):
     @line_magic
     def ed_config(self, parameter_s=''):
         self.shell.magic('ed {0}'.format(USER_CONFIG_PATH))
+
+    @line_magic
+    def ed_aliases(self, parameter_s=''):
+        self.shell.magic('ed {0}'.format(ALIAS_CONFIG_PATH))
 
     @line_magic
     def ed_editor(self, parameter_s=''):
