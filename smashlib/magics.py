0e0ebb42fa0918cd75d90767a9bfe4fe7b1a504e
mvr <matthewvonrocketstein@gmail-dot-com>
2014-12-20 01:10:46 +0000

    add uninstall_plugin magic

--- smashlib/magics.py
+++ smashlib/magics.py
@@ -3,6 +3,8 @@
 
 from IPython.core.magic import Magics, magics_class, line_magic
 
+from smashlib import get_smash
+
 from smashlib.data import USER_CONFIG_PATH, EDITOR_CONFIG_PATH
 from smashlib.data import ALIAS_CONFIG_PATH, MACRO_CONFIG_PATH
 
@@ -26,3 +28,8 @@ class SmashMagics(Magics):
     @line_magic
     def ed_editor(self, parameter_s=''):
         self.shell.magic('ed {0}'.format(EDITOR_CONFIG_PATH))
+
+    @line_magic
+    def uninstall_plugin(self, parameter_s=""):
+        plugin_name = parameter_s.strip().split()[-1]
+        get_smash()._installed_plugins[plugin_name].uninstall()
