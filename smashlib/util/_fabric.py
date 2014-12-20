f0716f256340cd40397c04d3fa4debdaa6b65171
mvr <matthewvonrocketstein@gmail-dot-com>
2014-12-20 01:09:59 +0000

    raise MissingSystemCommand instead of generic exception

--- smashlib/util/_fabric.py
+++ smashlib/util/_fabric.py
@@ -1,7 +1,11 @@
 """ smashlib.util.fabric
+
+    TODO: move to goulash
 """
 
 from fabric.api import quiet, local
+class MissingSystemCommand(RuntimeError):
+    pass
 
 def qlocal(*args, **kargs):
     with quiet():
@@ -14,4 +18,4 @@ def has_bin(name):
 def require_bin(name, msg=None):
     if not has_bin(name):
         msg = msg or "{0} is required".format(name)
-        raise Exception(msg)
+        raise MissingSystemCommand(msg)
